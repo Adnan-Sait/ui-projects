db3e70a91501067483d7436118c2671fafdb8c00
Adnan Sait <adnansait74@gmail.com>
2023-10-29 14:15:29 +0530

    Quotes: Filter initial commit

---
+++ quotes/src/components/QuotesFilter/QuotesFilter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./QuotesFilter.module.css";
+
+function QuotesFilter() {
+  return (
+    <div>
+      <div>Author</div>
+      <div>Tags</div>
+    </div>
+  );
+}
+
+export default QuotesFilter;
--- quotes/src/page/QuotesPage/QuotesPage.js
+++ quotes/src/page/QuotesPage/QuotesPage.js
@@ -4,6 +4,7 @@ import { quotesSelector } from "../../slices/quotesSlice";
 import styles from "./QuotesPage.module.css";
 import classNames from "classnames";
 import QuotesCard from "../../components/QuotesCard/QuotesCard";
+import QuotesFilter from "../../components/QuotesFilter/QuotesFilter";
 
 function QuotesPage() {
   /**
@@ -14,6 +15,9 @@ function QuotesPage() {
   return (
     <div className={classNames("container", styles.quotesPageContainer)}>
       <div className={styles.quotesGrid}>
+        <div className={styles.filterColumn}>
+          <QuotesFilter />
+        </div>
         {quotes.map((quoteItem) => {
           return <QuotesCard key={quoteItem.id} quote={quoteItem} />;
         })}
