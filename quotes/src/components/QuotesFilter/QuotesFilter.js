e7b17cb527ced1d19768389f093778f494a07249
Adnan Sait <adnansait74@gmail.com>
2023-11-11 15:58:03 +0530

    Quotes: Save filter fixed

--- quotes/src/components/QuotesFilter/QuotesFilter.js
+++ quotes/src/components/QuotesFilter/QuotesFilter.js
@@ -16,6 +16,9 @@ function QuotesFilter() {
   const { authors, categories } = useSelector(quotesSelector);
 
   /**
+   * Saves the filters into the query params.
+   * The current category is replaced with the selected filter values.
+   * The other filter categories are retained as is.
    *
    * @param {String} key                  Identifier for the filter.
    * @param {String[]} selectedFilters    Selected filter values.
@@ -29,19 +32,22 @@ function QuotesFilter() {
         filterValue = `${key}:${filterValue}`;
       }
 
+      // If other filter categories are already present in the query params, retain them.
       const currentFilters = state.get(FILTER_KEYS.filter);
       let filterArr = [];
 
       if (currentFilters) {
+        // Fetch all filter categories besides the current category.
         filterArr = currentFilters
           .split(FILTER_GROUP_SEPARATOR)
-          .filter((item) => !item.startsWith(`${key}:`));
+          .filter((item) => item && !item.startsWith(`${key}:`));
       }
 
       if (filterValue) {
         filterArr.push(filterValue);
       }
 
+      // If no filters exist, remove the filter query param.
       if (filterArr.length === 0) {
         state.delete(FILTER_KEYS.filter);
       } else {
