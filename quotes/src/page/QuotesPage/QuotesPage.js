69e46765f055765c69e1aa90d5fd6f82ee276e0f
Adnan Sait <adnansait74@gmail.com>
2023-11-12 20:39:10 +0530

    Quotes: Filtered quotes

--- quotes/src/page/QuotesPage/QuotesPage.js
+++ quotes/src/page/QuotesPage/QuotesPage.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { quotesSelector } from "../../store/slices/quotesSlice";
 import styles from "./QuotesPage.module.css";
 import classNames from "classnames";
 import QuotesCard from "../../components/QuotesCard/QuotesCard";
 import QuotesFilter from "../../components/QuotesFilter/QuotesFilter";
+import { selectedFiltersSelector } from "../../store/slices/filtersSlice";
 
 function QuotesPage() {
   /**
    * @type {{quotes: import("../../store/slices/quotesSlice").Quotes[]}}
    */
   const { quotes } = useSelector(quotesSelector);
+  /**
+   * @type {{author: string[], category: string[]}}
+   */
+  const { author: authorSelectedFilters, category: categorySelectedFilters } =
+    useSelector(selectedFiltersSelector);
+
+  const filteredQuotes = useMemo(filterQuotes, [
+    quotes,
+    authorSelectedFilters,
+    categorySelectedFilters,
+  ]);
+
+  /**
+   * Filters the quotes based on the selected filters.
+   *
+   * @returns {import("../../store/slices/quotesSlice").Quotes[]}
+   */
+  function filterQuotes() {
+    const authorCheck = authorSelectedFilters?.length > 0;
+    const categoryCheck = categorySelectedFilters?.length > 0;
+
+    return quotes.filter((item) => {
+      if (authorCheck && !authorSelectedFilters.includes(item.author)) {
+        return false;
+      }
+      if (
+        categoryCheck &&
+        !item.category.some((item) => categorySelectedFilters.includes(item))
+      ) {
+        return false;
+      }
+      return true;
+    });
+  }
 
   return (
     <div className={classNames("container", styles.quotesPageContainer)}>
@@ -18,7 +53,7 @@ function QuotesPage() {
         <div className={styles.filterColumn}>
           <QuotesFilter />
         </div>
-        {quotes.map((quoteItem) => {
+        {filteredQuotes.map((quoteItem) => {
           return <QuotesCard key={quoteItem.id} quote={quoteItem} />;
         })}
       </div>
